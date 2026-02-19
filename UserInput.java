9bd98af2910d890caf1e143b5ba9afb499765fc0
christiandickson7 <popepopeyes9@gmail.com>
2026-02-19 15:30:16 +0100

    ReadMe file

--- UserInput.java
+++ UserInput.java
@@ -13,6 +13,11 @@ public class UserInput{
 		System.out.print("Enter your age: ");
 		int age = scan.nextInt();
 		scan.nextLine();
+		System.out.print("Enter your gender(Male/female: ");
+		char gender = scan.next().charAt(0);
+		
+		System.out.printf("Are you happy to learn Java?(true/False: ");
+		boolean isHappy = scan.nextBoolean();
 		
 		System.out.println("");
 		System.out.println("");
@@ -20,6 +25,8 @@ public class UserInput{
 		System.out.printf("Hello %s ,How are you today?%n",fullName);
 		System.out.printf("Nice meeting you, you are living in %s%n",address);
 		System.out.printf("%s, you are %d years old",fullName,age);
+		System.out.printf("You are a %c ",gender);
+		System.out.printf("Are you happy to learn Java? %b%n ",isHappy);
 		
 		
 	}
